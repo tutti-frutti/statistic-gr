b389a2e73b01f59313fd502383b394e329568747
Anton <anton.verstka@gmail.com>
2016-08-10 18:16:53 +0300

    correcting

--- js/script.js
+++ js/script.js
@@ -36,18 +36,24 @@ $('.nav-left').on('click','.sub-true',function(e){   // меню слева
 //        $('nav-left__top').css('background-color', '#000');
 //    });
 
-//$('.nav-left__click').click(function () {
-//    $(this).closest('ul').css("background", "#ebe9e6");
-//    if($(this).hasClass('active')){
-//        $(this).removeClass('active');
-//        $(this).slideUp;
-//        $(this).closest('ul').css("background", "#f2f1ef");
-//    } else {
-//        
-//    }
-//    if($(this).hasClass('active')) {
-//        else
-//    }
-//        $(this).removeClass('active');
-//        $(this).closest('ul').css("background-color", "#f2f1ef");
-//});
+$('.nav-left__click').click(function () {
+//    $(this) // сейчас исполн всегда, переместить вниз
+    if($(this).hasClass('active')){
+        $(this).removeClass('active');
+        $(this).slideUp;
+        $(this).closest('ul').css({"background": "#f2f1ef",
+                                  "border": "0"});
+    } else {
+        $(this).addClass('active');
+        $(this).slideDown;
+        $(this).closest('ul').css({"background":"#ebe9e6",
+                                  "border-bottom": "2px solid #d8d8d8",
+                                  "border-top": "2px solid #d8d8d8"});
+    }
+    if($(this).hasClass('active')) {
+        $('.nav-left__click + a').css('text-decoration', 'underline');
+    }
+    
+
+});
+
